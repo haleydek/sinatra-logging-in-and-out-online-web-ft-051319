d01798f9eed9635711b0b68351df5440149f7809
Haley DeKeyser <hcd09@live.com>
2019-07-04 04:39:11 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers
   def self.current_user(session)
-    return User.find(session[:user_id])
+    User.find(session[:user_id])
+    binding.pry
   end
   
   def self.is_logged_in?(session)
